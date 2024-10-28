f231d5929adaf7084b33a1a5cb22e6d282b29d26
bangyewon <90364660+bangyewon@users.noreply.github.com>
2024-10-28 22:04:06 +0900

    [add] 개발환경 셋팅

---
+++ src/main/java/classfit/example/classfit/ClassfitApplication.java
@@ -0,0 +1,13 @@
+package classfit.example.classfit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClassfitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClassfitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/classfit/example/classfit/ClassfitApplicationTests.java
@@ -0,0 +1,13 @@
+package classfit.example.classfit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClassfitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
