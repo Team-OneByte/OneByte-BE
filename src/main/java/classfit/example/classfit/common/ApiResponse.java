68ea8eb464fa714a2a0c1fadfecec586bae18d24
bangyewon <90364660+bangyewon@users.noreply.github.com>
2024-11-10 22:23:04 +0900

    [refactor] 상태코드 수정 #8

--- src/main/java/classfit/example/classfit/common/ApiResponse.java
+++ src/main/java/classfit/example/classfit/common/ApiResponse.java
@@ -3,13 +3,16 @@ package classfit.example.classfit.common;
 import com.fasterxml.jackson.annotation.JsonInclude;
 
 @JsonInclude(JsonInclude.Include.NON_NULL)
-public record ApiResponse<T>(ResultType resultType, T success, ErrorResult error) {
-    public static <T> ApiResponse<T> success(T data) {
-        return new ApiResponse<>(ResultType.SUCCESS, data, null);
+public record ApiResponse<T>(int statusCode, ResultType resultType, T data, ErrorResult error) {
+
+    // success 메서드: message는 추가할 수 있지만, null로 하지 않음
+    public static <T> ApiResponse<T> success(T data, int statusCode, String message) {
+        return new ApiResponse<>(statusCode, ResultType.SUCCESS, data, null);
     }
 
-    public static ApiResponse<?> fail(String errorMessage) {
-        return new ApiResponse<>(ResultType.FAIL, null, new ErrorResult(errorMessage));
+    // fail 메서드: errorMessage를 ErrorResult로만 전달
+    public static ApiResponse<?> fail(int statusCode, String errorMessage) {
+        return new ApiResponse<>(statusCode, ResultType.FAIL, null, new ErrorResult(errorMessage));
     }
 
     private record ErrorResult(String message) {
