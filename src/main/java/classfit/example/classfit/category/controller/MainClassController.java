03a6599ec78f9e6cda719e46a203c0e648613d8d
bangyewon <90364660+bangyewon@users.noreply.github.com>
2024-11-11 22:13:01 +0900

    [chore] 스웨거 설명 추가 #8

--- src/main/java/classfit/example/classfit/category/controller/MainClassController.java
+++ src/main/java/classfit/example/classfit/category/controller/MainClassController.java
@@ -4,6 +4,8 @@ import classfit.example.classfit.category.dto.request.MainClassRequest;
 import classfit.example.classfit.category.dto.response.MainClassResponse;
 import classfit.example.classfit.category.service.MainClassService;
 import classfit.example.classfit.common.ApiResponse;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
@@ -17,11 +19,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api/v1/home")
 @RequiredArgsConstructor
+@Tag(name = "메인 클래스 컨트롤러", description = "메인 클래스 관련 API입니다.")
 public class MainClassController {
 
     private final MainClassService mainClassService;
 
     @PostMapping("/main-category")
+    @Operation(summary = "메인 클래스 추가", description = "메인 클래스 추가하는 api 입니다.")
     public ApiResponse<MainClassResponse> addMainClass(
             @RequestHeader("member-no") Long memberId,
             @RequestBody MainClassRequest req
@@ -30,6 +34,7 @@ public class MainClassController {
     }
 
     @PatchMapping("/main-category/{mainClassId}")
+    @Operation(summary = "메인 클래스 수정", description = "메인 클래스의 이름을 수정하는 api 입니다.")
     public ApiResponse<MainClassResponse> updateMainClass(@RequestHeader("member-no") Long memberId,
             @PathVariable(name = "mainClassId") Long mainClassId,
             @RequestBody MainClassRequest req) {
@@ -37,6 +42,7 @@ public class MainClassController {
     }
 
     @DeleteMapping("/main-category/{mainClassId}")
+    @Operation(summary = "메인 클래스 삭제", description = "메인 클래스 삭제하는 api 입니다.")
     public ApiResponse<?> deleteMainClass(@RequestHeader("member-no") Long memberId,
             @PathVariable(name = "mainClassId") Long mainClassId) {
         return mainClassService.deleteMainClass(memberId, mainClassId);
--- src/main/java/classfit/example/classfit/category/controller/SubClassController.java
+++ src/main/java/classfit/example/classfit/category/controller/SubClassController.java
@@ -4,6 +4,8 @@ import classfit.example.classfit.category.dto.request.SubClassRequest;
 import classfit.example.classfit.category.dto.response.SubClassResponse;
 import classfit.example.classfit.category.service.SubClassService;
 import classfit.example.classfit.common.ApiResponse;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
@@ -17,17 +19,20 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api/v1/home")
 @RequiredArgsConstructor
+@Tag(name = "하위 클래스 컨트롤러", description = "하위 클래스 관련 API입니다.")
 public class SubClassController {
 
     private final SubClassService subClassService;
 
     @PostMapping("/sub-category")
+    @Operation(summary = "하위 클래스 추가", description = "하위 클래스 추가하는 api 입니다.")
     public ApiResponse<SubClassResponse> addSubClass(@RequestHeader("member-no") Long memberId,
             @RequestBody SubClassRequest req) {
         return subClassService.addSubClass(memberId, req);
     }
 
     @PatchMapping("/sub-category/{subClassId}")
+    @Operation(summary = "하위 클래스 수정", description = "하위 클래스 이름을 수정하는 api 입니다.")
     public ApiResponse<SubClassResponse> updateSubClass(
             @RequestHeader(name = "member-no") Long memberId,
             @PathVariable(name = "subClassId") Long subClassId,
@@ -36,6 +41,7 @@ public class SubClassController {
     }
 
     @DeleteMapping("/sub-category/{subClassId}")
+    @Operation(summary = "하위 클래스 삭제", description = "하위 클래스 삭제하는 api 입니다.")
     public ApiResponse<?> deleteSubClass(
             @RequestHeader(name = "member-no") Long memberId,
             @PathVariable(name = "subClassId") Long subClassId) {
