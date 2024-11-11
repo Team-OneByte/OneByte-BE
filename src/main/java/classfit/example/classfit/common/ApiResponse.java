2a134e32a5b4cfba6a76dbe66e5b0b196fb33188
bangyewon <90364660+bangyewon@users.noreply.github.com>
2024-11-11 22:16:54 +0900

    [chore] 스웨거 설명 추가 #8

--- src/main/java/classfit/example/classfit/common/ApiResponse.java
+++ src/main/java/classfit/example/classfit/common/ApiResponse.java
@@ -5,12 +5,10 @@ import com.fasterxml.jackson.annotation.JsonInclude;
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public record ApiResponse<T>(int statusCode, ResultType resultType, T data, ErrorResult error) {
 
-    // success 메서드: message는 추가할 수 있지만, null로 하지 않음
     public static <T> ApiResponse<T> success(T data, int statusCode, String message) {
         return new ApiResponse<>(statusCode, ResultType.SUCCESS, data, null);
     }
 
-    // fail 메서드: errorMessage를 ErrorResult로만 전달
     public static ApiResponse<?> fail(int statusCode, String errorMessage) {
         return new ApiResponse<>(statusCode, ResultType.FAIL, null, new ErrorResult(errorMessage));
     }
