4906c1dc3bc8cfed58f32310e7d29558cc3b6379
dpfls0922 <dpfls0922@naver.com>
2024-11-12 15:02:29 +0900

    [feat] 클래스 정보 조회 #19

--- src/main/java/classfit/example/classfit/category/repository/MainClassRespository.java
+++ src/main/java/classfit/example/classfit/category/repository/MainClassRespository.java
@@ -4,7 +4,9 @@ import classfit.example.classfit.domain.MainClass;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface MainClassRespository extends JpaRepository<MainClass,Long> {
-
+    List<MainClass> findAll();
 }
