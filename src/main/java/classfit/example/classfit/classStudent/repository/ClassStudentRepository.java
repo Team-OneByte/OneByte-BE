f810893b5fdc313db11d2591f89d10fe85233413
jaehyuk <bjh010321@naver.com>
2024-11-13 22:49:46 +0900

    [fix] 어노테이션 추가

--- src/main/java/classfit/example/classfit/classStudent/repository/ClassStudentRepository.java
+++ src/main/java/classfit/example/classfit/classStudent/repository/ClassStudentRepository.java
@@ -7,7 +7,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ClassStudentRepository extends JpaRepository<ClassStudent, Long> {
 
     Page<ClassStudent> findBySubClass_MainClass_IdAndSubClass_Id(Long mainClassId, Long subClassId,
