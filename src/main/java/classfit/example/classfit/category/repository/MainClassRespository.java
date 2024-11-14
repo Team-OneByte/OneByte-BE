b059d9584dfd1e381775e81aedb7da3152b7d52f
bangyewon <90364660+bangyewon@users.noreply.github.com>
2024-11-14 23:39:06 +0900

    [refactor] mainClass 이름 중복 검증 추가

--- src/main/java/classfit/example/classfit/category/repository/MainClassRespository.java
+++ src/main/java/classfit/example/classfit/category/repository/MainClassRespository.java
@@ -1,6 +1,7 @@
 package classfit.example.classfit.category.repository;
 
 import classfit.example.classfit.domain.MainClass;
+import classfit.example.classfit.domain.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -9,4 +10,6 @@ import java.util.List;
 @Repository
 public interface MainClassRespository extends JpaRepository<MainClass,Long> {
     List<MainClass> findAllByOrderByMainClassNameAsc();
+
+    boolean existsByMemberAndMainClassName(Member member, String mainClassName);
 }
