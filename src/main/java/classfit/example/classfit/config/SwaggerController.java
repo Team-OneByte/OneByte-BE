00c9589e5c6cf86969e0bfef85ab29ac85f30db7
bangyewon <90364660+bangyewon@users.noreply.github.com>
2024-11-15 17:32:17 +0900

    [refactor] 스웨거 경로 변경

--- src/main/java/classfit/example/classfit/config/SwaggerController.java
+++ src/main/java/classfit/example/classfit/config/SwaggerController.java
@@ -12,17 +12,17 @@ public class SwaggerController {
                 "  <head>\n" +
                 "    <meta charset=\"UTF-8\">\n" +
                 "    <title>Swagger UI</title>\n" +
-                "    <link rel=\"stylesheet\" type=\"text/css\" href=\"http://15.164.254.208:8080/swagger-ui/swagger-ui.css\" />\n" +
-                "    <link rel=\"stylesheet\" type=\"text/css\" href=\"http://15.164.254.208:8080/swagger-ui/index.css\" />\n" +
-                "    <link rel=\"icon\" type=\"image/png\" href=\"http://15.164.254.208:8080/swagger-ui/favicon-32x32.png\" sizes=\"32x32\" />\n" +
-                "    <link rel=\"icon\" type=\"image/png\" href=\"http://15.164.254.208:8080/swagger-ui/favicon-16x16.png\" sizes=\"16x16\" />\n" +
+                "    <link rel=\"stylesheet\" type=\"text/css\" href=\"https://classfit.duckdns.org/swagger-ui/swagger-ui.css\" />\n" +
+                "    <link rel=\"stylesheet\" type=\"text/css\" href=\"https://classfit.duckdns.org/swagger-ui/index.css\" />\n" +
+                "    <link rel=\"icon\" type=\"image/png\" href=\"https://classfit.duckdns.org/swagger-ui/favicon-32x32.png\" sizes=\"32x32\" />\n" +
+                "    <link rel=\"icon\" type=\"image/png\" href=\"https://classfit.duckdns.org/swagger-ui/favicon-16x16.png\" sizes=\"16x16\" />\n" +
                 "  </head>\n" +
                 "\n" +
                 "  <body>\n" +
                 "    <div id=\"swagger-ui\"></div>\n" +
-                "    <script src=\"http://15.164.254.208:8080/swagger-ui/swagger-ui-bundle.js\" charset=\"UTF-8\"> </script>\n" +
-                "    <script src=\"http://15.164.254.208:8080/swagger-ui/swagger-ui-standalone-preset.js\" charset=\"UTF-8\"> </script>\n" +
-                "    <script src=\"http://15.164.254.208:8080/swagger-ui/swagger-initializer.js\" charset=\"UTF-8\"> </script>\n" +
+                "    <script src=\"https://classfit.duckdns.org/swagger-ui/swagger-ui-bundle.js\" charset=\"UTF-8\"> </script>\n" +
+                "    <script src=\"https://classfit.duckdns.org/swagger-ui/swagger-ui-standalone-preset.js\" charset=\"UTF-8\"> </script>\n" +
+                "    <script src=\"https://classfit.duckdns.org/swagger-ui/swagger-initializer.js\" charset=\"UTF-8\"> </script>\n" +
                 "  </body>\n" +
                 "</html>\n";
     }
