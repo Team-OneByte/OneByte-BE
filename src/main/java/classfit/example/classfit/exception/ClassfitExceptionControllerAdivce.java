05986c669b00aceb79596a7db2814a7dd02da5c0
백재혁 <bjh010321@naver.com>
2024-11-20 19:50:01 +0900

    [feat] HttpMessageNotReadableException 예외 처리 추가 #61

--- src/main/java/classfit/example/classfit/exception/ClassfitExceptionControllerAdivce.java
+++ src/main/java/classfit/example/classfit/exception/ClassfitExceptionControllerAdivce.java
@@ -1,9 +1,11 @@
 package classfit.example.classfit.exception;
 
 import classfit.example.classfit.common.ApiResponse;
+import com.fasterxml.jackson.databind.exc.MismatchedInputException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -30,6 +32,21 @@ public class ClassfitExceptionControllerAdivce {
         log.error("Validation error: {}", errorMessage);
         return ResponseEntity.badRequest()
             .body(ApiResponse.fail(HttpStatus.BAD_REQUEST.value(), errorMessage));
+    }
+
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    public ResponseEntity<ApiResponse<?>> handleHttpMessageNotReadable(HttpMessageNotReadableException e) {
+        log.error("Exception", e);
+
+        if (e.getCause() instanceof MismatchedInputException mismatchedInputException) {
+            return ResponseEntity.badRequest()
+                .body(ApiResponse.fail(HttpStatus.BAD_REQUEST.value(),
+                    mismatchedInputException.getPath().get(0).getFieldName() + " 필드의 값이 잘못되었습니다."));
+        }
+
+        return ResponseEntity.badRequest()
+            .body(ApiResponse.fail(HttpStatus.BAD_REQUEST.value(),
+                "확인할 수 없는 형태의 데이터가 들어왔습니다"));
     }
 
     @ExceptionHandler(Exception.class)
