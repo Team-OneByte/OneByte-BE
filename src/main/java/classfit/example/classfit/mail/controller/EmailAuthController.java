cb9e924f81755dbe365c31cce04089c1a6f881ed
백재혁 <bjh010321@naver.com>
2024-11-25 18:41:25 +0900

    [chore] 스웨거 설명 추가 #65

--- src/main/java/classfit/example/classfit/mail/controller/EmailAuthController.java
+++ src/main/java/classfit/example/classfit/mail/controller/EmailAuthController.java
@@ -5,6 +5,8 @@ import classfit.example.classfit.mail.dto.request.EmailAuthRequest;
 import classfit.example.classfit.mail.dto.request.EmailAuthVerifyRequest;
 import classfit.example.classfit.mail.dto.response.EmailAuthResponse;
 import classfit.example.classfit.mail.service.EmailAuthService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
@@ -12,17 +14,20 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/mail")
+@Tag(name = "이메일 API", description = "이메일 관련 API")
 public class EmailAuthController {
 
     private final EmailAuthService emailAuthService;
 
     @GetMapping("/send")
+    @Operation(summary = "이메일 전송", description = "특정 이메일에 인증코드를 전송하는 API 입니다.")
     public ApiResponse<EmailAuthResponse> sendMail(@RequestBody @Valid EmailAuthRequest request) {
         EmailAuthResponse emailAuthResponse = emailAuthService.sendEmail(request);
         return ApiResponse.success(emailAuthResponse, 200, "이메일이 전송되었습니다");
     }
 
     @PostMapping("/verify")
+    @Operation(summary = "이메일 인증코드 검증", description = "인증코드를 검증하는 API 입니다.")
     public ApiResponse<EmailAuthResponse> verifyMail(@RequestBody @Valid EmailAuthVerifyRequest request) {
         EmailAuthResponse emailAuthResponse = emailAuthService.verifyAuthCode(request);
         return ApiResponse.success(emailAuthResponse, 200, "정상적으로 확인되었습니다.");
