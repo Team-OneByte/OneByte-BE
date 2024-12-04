bbc94207601e741306dfae6bdf04bb66d9d7929a
백재혁 <bjh010321@naver.com>
2024-12-04 19:58:00 +0900

    [feat] CustomAuthenticationToken 생성 및 적용 #97
    - UsernamePasswordAuthenticationToken을 확장한 CustomAuthenticationToken 생성
    - email 필드를 추가하여 사용자 인증 정보에 이메일 정보를 포함
    - CustomAuthenticationToken이 CustomAuthenticationProvider에서 처리될 수 있도록 설계

---
+++ src/main/java/classfit/example/classfit/auth/dto/request/CustomAuthenticationToken.java
@@ -0,0 +1,19 @@
+package classfit.example.classfit.auth.dto.request;
+
+import lombok.Getter;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.GrantedAuthority;
+
+import java.util.Collection;
+
+@Getter
+public class CustomAuthenticationToken extends UsernamePasswordAuthenticationToken {
+
+    private final String email;
+
+    public CustomAuthenticationToken(String email, Object credentials, Collection<? extends GrantedAuthority> authorities) {
+        super(email, credentials, authorities);
+        this.email = email;
+    }
+}
+
