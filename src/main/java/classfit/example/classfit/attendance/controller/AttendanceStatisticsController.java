5d885dba9909eb7f6e6cbbf6ea510367df3f9b0c
dpfls0922 <dpfls0922@naver.com>
2024-12-06 14:51:51 +0900

    [refactor] 공통 uri 관리 #90

--- src/main/java/classfit/example/classfit/attendance/controller/AttendanceStatisticsController.java
+++ src/main/java/classfit/example/classfit/attendance/controller/AttendanceStatisticsController.java
@@ -14,13 +14,13 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/v1/home")
+@RequestMapping("/api/v1/home/statistics")
 @RequiredArgsConstructor
 @Tag(name = "출결 통계 컨트롤러", description = "출결 통계 관련 API입니다.")
 public class AttendanceStatisticsController {
     private final AttendanceStatisticsService attendanceStatisticsService;
 
-    @GetMapping("/statistics/date")
+    @GetMapping("/date")
     @Operation(summary = "클래스 날짜별 통계", description = "날짜별 출결 통계 조회 시 사용되는 api 입니다.")
     public ApiResponse<List<StatisticsDateResponse>> getAttendanceStaticsByDate(
         @Parameter(description = "조회 시작 기간")
@@ -34,7 +34,7 @@ public class AttendanceStatisticsController {
         return ApiResponse.success(statisticsDate, 200, "SUCCESS");
     }
 
-    @GetMapping("/statistics/member")
+    @GetMapping("/member")
     @Operation(summary = "클래스 구성원별 통계", description = "구성원별 출결 통계 조회 시 사용되는 api 입니다.")
     public ApiResponse getAttendanceStaticsByMember(
         @Parameter(description = "조회 시작 기간")
