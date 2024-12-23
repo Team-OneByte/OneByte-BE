544c005c1170fc107b7446f90544ae55f6cde063
백재혁 <bjh010321@naver.com>
2024-12-23 17:47:20 +0900

    [ci/cd] Git Action 스크립트 수정

--- src/main/java/classfit/example/classfit/common/util/RedisUtil.java
+++ src/main/java/classfit/example/classfit/common/util/RedisUtil.java
@@ -27,7 +27,7 @@ public class RedisUtil {
 
             return value;
         } catch (DataAccessException e) {
-            throw new ClassfitException("Redis 서버에서 데이터를 가져오는 중 오류가 발생했습니다: ", HttpStatus.INTERNAL_SERVER_ERROR);
+            throw new ClassfitException("Redis 서버에서 데이터를 가져오는 중 오류가 발생했습니다: " + e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
 
