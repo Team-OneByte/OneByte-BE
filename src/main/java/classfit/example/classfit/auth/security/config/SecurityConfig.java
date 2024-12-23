c3ff47224de855f2eefdc652378afd0b2860b16f
백재혁 <bjh010321@naver.com>
2024-12-23 15:33:24 +0900

    [ci/cd] Git Action 스크립트 수정

--- src/main/java/classfit/example/classfit/auth/security/config/SecurityConfig.java
+++ src/main/java/classfit/example/classfit/auth/security/config/SecurityConfig.java
@@ -61,7 +61,7 @@ public class SecurityConfig {
 
         security
             .authorizeHttpRequests((auth) -> auth
-                .requestMatchers("/", "/swagger-ui/**", "/v3/api-docs/**").permitAll()  // 스웨거 관련 엔드포인트 허용
+                .requestMatchers("http://210.107.205.122:20030/swagger-ui/index.html/", "http://210.107.205.122:20030/swagger-ui/**", "http://210.107.205.122:20030/v3/api-docs/**").permitAll()  // 스웨거 관련 엔드포인트 허용
                 .requestMatchers("/api/v1/login", "/api/v1/sign_in", "/api/v1/reissue", "/api/v1/mail/send", "/api/v1/mail/verify").permitAll()
                 .requestMatchers("/api/v1/academy/code", "/api/v1/academy/register").permitAll()
                 .requestMatchers("/api/v1/**").hasAnyRole("MEMBER", "ADMIN")
