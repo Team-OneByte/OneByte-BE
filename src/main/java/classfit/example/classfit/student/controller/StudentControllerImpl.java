8e36b560b9b086a9c5852da0d57516767d64c1e8
백재혁 <bjh010321@naver.com>
2024-12-30 12:20:07 +0900

    [refactor] 스웨거 태그 수정 및 엔드포인트 수정

--- src/main/java/classfit/example/classfit/student/controller/StudentControllerImpl.java
+++ src/main/java/classfit/example/classfit/student/controller/StudentControllerImpl.java
@@ -9,22 +9,15 @@ import classfit.example.classfit.student.service.StudentService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
-import java.util.List;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PatchMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/student")
 @RequiredArgsConstructor
-@Tag(name = "학생 API", description = "학생 관련 API")
+@Tag(name = "학생 컨트롤러", description = "학생 관련 API")
 public class StudentControllerImpl {
 
     private final StudentService studentService;
