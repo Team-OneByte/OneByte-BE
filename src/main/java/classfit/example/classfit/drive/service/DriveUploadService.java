a9a7d85ea350e8d212e0398fa63d0ac223a0651c
dpfls0922 <dpfls0922@naver.com>
2025-01-03 16:49:55 +0900

    [refactor] 개별 파일 업로드 리팩토링 #135

--- src/main/java/classfit/example/classfit/drive/service/DriveUploadService.java
+++ src/main/java/classfit/example/classfit/drive/service/DriveUploadService.java
@@ -1,13 +1,17 @@
 package classfit.example.classfit.drive.service;
 
+import classfit.example.classfit.common.exception.ClassfitException;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.model.ObjectMetadata;
 import com.amazonaws.services.s3.model.PutObjectRequest;
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.UUID;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -20,12 +24,22 @@ public class DriveUploadService {
     private String bucketName;
 
     public String uploadFile(MultipartFile file) throws IOException {
-        String objectKey = generateObjectKey(file);
+        return uploadToS3(file);
+    }
+
+    private String uploadToS3(MultipartFile file) throws IOException {
         try (InputStream inputStream = file.getInputStream()) {
-            ObjectMetadata metadata = buildObjectMetadata(file);
-            PutObjectRequest putObjectRequest = new PutObjectRequest(bucketName, objectKey, inputStream, metadata);
-            amazonS3.putObject(putObjectRequest);
+            return uploadAndGetUrl(inputStream, file);
+        } catch (IOException e) {
+            throw new ClassfitException("파일 업로드 오류: " + file.getOriginalFilename(), HttpStatus.BAD_REQUEST);
         }
+    }
+
+    private String uploadAndGetUrl(InputStream inputStream, MultipartFile file) {
+        String objectKey = generateObjectKey(file);
+        ObjectMetadata metadata = buildObjectMetadata(file);
+        PutObjectRequest putObjectRequest = new PutObjectRequest(bucketName, objectKey, inputStream, metadata);
+        amazonS3.putObject(putObjectRequest);
         return amazonS3.getUrl(bucketName, objectKey).toString();
     }
 
