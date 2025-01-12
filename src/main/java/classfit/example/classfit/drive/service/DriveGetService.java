aa1bf2a0cf9d4cd6bee2708ac5636b300eb5584e
dpfls0922 <dpfls0922@naver.com>
2025-01-12 12:58:19 +0900

    [refactor] 응답 형식 통일 #163

--- src/main/java/classfit/example/classfit/drive/service/DriveGetService.java
+++ src/main/java/classfit/example/classfit/drive/service/DriveGetService.java
@@ -1,20 +1,21 @@
 package classfit.example.classfit.drive.service;
 
 import classfit.example.classfit.common.exception.ClassfitException;
+import classfit.example.classfit.common.util.DriveUtil;
 import classfit.example.classfit.drive.domain.DriveType;
 import classfit.example.classfit.drive.domain.FileType;
-import classfit.example.classfit.drive.dto.response.FileInfo;
+import classfit.example.classfit.drive.dto.response.FileResponse;
 import classfit.example.classfit.member.domain.Member;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.model.*;
 import lombok.RequiredArgsConstructor;
+import org.jetbrains.annotations.NotNull;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
 import java.text.Normalizer;
 import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
@@ -28,19 +29,19 @@ public class DriveGetService {
     @Value("${cloud.aws.s3.bucket}")
     private String bucketName;
 
-    public List<FileInfo> getFilesFromS3(Member member, DriveType driveType, String folderPath) {
-        List<FileInfo> files = new ArrayList<>();
+    public List<FileResponse> getFilesFromS3(Member member, DriveType driveType, String folderPath) {
+        List<FileResponse> files = new ArrayList<>();
         String prefix = getPrefixByDriveType(member, driveType, folderPath);
         List<S3ObjectSummary> objectSummaries = getS3ObjectList(prefix);
 
         for (S3ObjectSummary summary : objectSummaries) {
-            FileInfo fileInfo = buildFileInfo(summary);
+            FileResponse fileInfo = buildFileInfo(summary);
             files.add(fileInfo);
         }
         return files;
     }
 
-    public List<FileInfo> searchFilesByName(Member member, DriveType driveType, String fileName) {
+    public List<FileResponse> searchFilesByName(Member member, DriveType driveType, String fileName) {
         ListObjectsV2Request request = createListObjectsRequest(driveType, member, fileName);
         ListObjectsV2Result result = amazonS3.listObjectsV2(request);
 
@@ -78,15 +79,31 @@ public class DriveGetService {
         return result.getObjectSummaries();
     }
 
-    private FileInfo buildFileInfo(S3ObjectSummary summary) {
+    private FileResponse buildFileInfo(S3ObjectSummary summary) {
         String fileName = summary.getKey();
         String fileUrl = amazonS3.getUrl(bucketName, fileName).toString();
 
         Map<String, String> tagMap = getTagsForS3Object(fileName);
-        String folderPath = tagMap.get("folderPath");
-        String uploadedBy = tagMap.get("uploadedBy");
-        LocalDateTime uploadedAt = parseUploadedAt(tagMap.get("uploadedAt"));
-        return new FileInfo(fileName, fileUrl, folderPath, uploadedBy, uploadedAt);
+
+        return getFileResponse(summary, fileName, fileUrl, tagMap);
+    }
+
+    @NotNull
+    static FileResponse getFileResponse(S3ObjectSummary summary, String fileName, String fileUrl,
+        Map<String, String> tagMap) {
+        FileType fileType = DriveUtil.getFileType(fileName);
+        LocalDateTime uploadedAt = DriveUtil.parseUploadedAt(tagMap.get("uploadedAt"));
+        String fileSize = DriveUtil.formatFileSize(summary.getSize());
+
+        return new FileResponse(
+            fileType,
+            fileName,
+            fileSize,
+            fileUrl,
+            tagMap.getOrDefault("folderPath", ""),
+            tagMap.getOrDefault("uploadedBy", ""),
+            uploadedAt
+        );
     }
 
     private Map<String, String> getTagsForS3Object(String objectKey) {
@@ -95,13 +112,6 @@ public class DriveGetService {
             .collect(Collectors.toMap(Tag::getKey, Tag::getValue));
     }
 
-    private LocalDateTime parseUploadedAt(String uploadedAtStr) {
-        if (uploadedAtStr == null || uploadedAtStr.trim().isEmpty()) {
-            return LocalDateTime.now();
-        }
-        DateTimeFormatter formatter = DateTimeFormatter.ISO_LOCAL_DATE_TIME;
-        return LocalDateTime.parse(uploadedAtStr, formatter);
-    }
 
     private ListObjectsV2Request createListObjectsRequest(DriveType driveType, Member member, String folderPath) {
         ListObjectsV2Request request = new ListObjectsV2Request()
@@ -131,13 +141,13 @@ public class DriveGetService {
         return Normalizer.normalize(input, Normalizer.Form.NFC).trim().toLowerCase();
     }
 
-    public List<FileInfo> classifyFilesByType(Member member, DriveType driveType, FileType filterFileType) {
+    public List<FileResponse> classifyFilesByType(Member member, DriveType driveType, FileType filterFileType) {
         ListObjectsV2Request request = createListObjectsRequest(driveType, member, "");
         ListObjectsV2Result result = amazonS3.listObjectsV2(request);
 
         return result.getObjectSummaries().stream()
             .map(this::buildFileInfo)
-            .filter(fileInfo -> !isFolder(fileInfo))  // 폴더는 제외
+            .filter(fileInfo -> !isFolder(fileInfo))     // 폴더는 제외
             .filter(fileInfo -> {
                 FileType fileType = getFileType(fileInfo.fileName());
                 return fileType.equals(filterFileType);  // 필터링 조건 추가
@@ -145,7 +155,7 @@ public class DriveGetService {
             .collect(Collectors.toList());
     }
 
-    private boolean isFolder(FileInfo fileInfo) {
+    private boolean isFolder(FileResponse fileInfo) {
         return fileInfo.fileName().endsWith("/");
     }
 
