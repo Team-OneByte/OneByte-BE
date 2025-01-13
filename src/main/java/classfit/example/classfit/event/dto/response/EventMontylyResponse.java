d27999d853040cdd83c5cd4313750bb6003ae8cb
dpfls0922 <dpfls0922@naver.com>
2025-01-14 01:10:29 +0900

    [refactor] 월별 일정 조회 시 카테고리 색상 추가 #182

--- src/main/java/classfit/example/classfit/event/dto/response/EventMontylyResponse.java
+++ src/main/java/classfit/example/classfit/event/dto/response/EventMontylyResponse.java
@@ -4,11 +4,12 @@ public record EventMontylyResponse
     (
         String id,
         String name,
+        String color,
         String eventType,
         String startDate,
         String endDate
     ) {
-    public static EventMontylyResponse of(final String id, final String name, final String eventType, final String startDate, final String endDate) {
-        return new EventMontylyResponse(id, name, eventType, startDate, endDate);
+    public static EventMontylyResponse of(final String id, final String name, final String color, final String eventType, final String startDate, final String endDate) {
+        return new EventMontylyResponse(id, name, color, eventType, startDate, endDate);
     }
 }
