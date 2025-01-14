5376de5f5289cec2d161967b8e92f8efd3075172
백재혁 <bjh010321@naver.com>
2025-01-15 03:47:11 +0900

    [hotfix] 클래스 정보 조회 오류 수정

--- src/main/java/classfit/example/classfit/category/repository/MainClassRepository.java
+++ src/main/java/classfit/example/classfit/category/repository/MainClassRepository.java
@@ -2,7 +2,6 @@ package classfit.example.classfit.category.repository;
 
 import classfit.example.classfit.academy.domain.Academy;
 import classfit.example.classfit.category.domain.MainClass;
-import classfit.example.classfit.member.domain.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -10,7 +9,7 @@ import java.util.List;
 
 @Repository
 public interface MainClassRepository extends JpaRepository<MainClass, Long> {
-    List<MainClass> findAllByOrderByMainClassNameAsc();
+    List<MainClass> findAllByAcademyOrderByMainClassNameAsc(Academy academy);
 
     List<MainClass> findByAcademy(Academy academy);
 
