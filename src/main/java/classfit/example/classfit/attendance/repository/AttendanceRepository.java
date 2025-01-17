c0ef3405fbd5f103ed0b52bbdfe84e4ce3a1c1dc
dpfls0922 <dpfls0922@naver.com>
2025-01-17 20:58:05 +0900

    [fix] 엑셀 출결 데이터 조회 API에 로그인 멤버 학원 검증 로직 추가 #227

--- src/main/java/classfit/example/classfit/attendance/repository/AttendanceRepository.java
+++ src/main/java/classfit/example/classfit/attendance/repository/AttendanceRepository.java
@@ -9,6 +9,7 @@ import org.springframework.data.jpa.repository.Query;
 import java.time.LocalDate;
 import java.util.List;
 import java.util.Optional;
+import org.springframework.data.repository.query.Param;
 
 public interface AttendanceRepository extends JpaRepository<Attendance, Long> {
 
@@ -17,11 +18,20 @@ public interface AttendanceRepository extends JpaRepository<Attendance, Long> {
     @Query("SELECT MAX(a.date) FROM Attendance a")
     Optional<LocalDate> findLastGeneratedDate();
 
-    @Query("SELECT a FROM Attendance a WHERE a.student IN (SELECT cs.student FROM ClassStudent cs WHERE cs.subClass.id = :subClassId) AND FUNCTION('MONTH', a.date) = :month")
-    List<Attendance> findBySubClassIdAndMonth(Long subClassId, int month);
+    @Query("SELECT a FROM Attendance a WHERE a.student IN " +
+        "(SELECT cs.student FROM ClassStudent cs WHERE cs.subClass.id = :subClassId AND cs.subClass.mainClass.academy.id =:academyId) " +
+        "AND FUNCTION('MONTH', a.date) = :month")
+    List<Attendance> findByAcademyIdAndSubClassIdAndMonth(
+        @Param("academyId") Long academyId,
+        @Param("subClassId") Long subClassId,
+        @Param("month") int month);
 
-    @Query("SELECT a FROM Attendance a WHERE FUNCTION('MONTH', a.date) = :month")
-    List<Attendance> findByMonth(int month);
+    @Query("SELECT a FROM Attendance a WHERE a.student IN " +
+        "(SELECT cs.student FROM ClassStudent cs WHERE cs.subClass.mainClass.academy.id =:academyId) " +
+        "AND FUNCTION('MONTH', a.date) = :month")
+    List<Attendance> findByAcademyIdAndMonth(
+        @Param("academyId") Long academyId,
+        @Param("month") int month);
 
     @Query("SELECT a FROM Attendance a JOIN a.classStudent cs WHERE cs.subClass.id = :subClassId AND a.date BETWEEN :startDate AND :endDate")
     List<Attendance> findByDateBetweenAndSubClassId(LocalDate startDate, LocalDate endDate,
