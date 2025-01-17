7a2f23b119c29fdc08c6e6174f67aee90a6e89e8
bangyewon <90364660+bangyewon@users.noreply.github.com>
2025-01-18 05:36:31 +0900

    [fix] 학습리포트 작성자 추가 #241

--- src/main/java/classfit/example/classfit/scoreReport/dto/response/FindAllReportResponse.java
+++ src/main/java/classfit/example/classfit/scoreReport/dto/response/FindAllReportResponse.java
@@ -3,7 +3,7 @@ package classfit.example.classfit.scoreReport.dto.response;
 import java.time.LocalDate;
 
 public record FindAllReportResponse(Long studentReportId, Long studentId, String studentName,
-                                    String reportName, String ReportCreatedByName,
+                                    String reportName, String ReportCreatedBy,
                                     LocalDate createAt) {
 
 }
--- src/main/java/classfit/example/classfit/scoreReport/dto/response/FindReportResponse.java
+++ src/main/java/classfit/example/classfit/scoreReport/dto/response/FindReportResponse.java
@@ -3,7 +3,7 @@ package classfit.example.classfit.scoreReport.dto.response;
 import java.time.LocalDate;
 
 public record FindReportResponse(Long studentReportId, Long studentId, String studentName,
-                                 String reportName, String reportCreatedByName,
+                                 String reportName, String reportCreatedBy,
                                  LocalDate createAt) {
 
 }
