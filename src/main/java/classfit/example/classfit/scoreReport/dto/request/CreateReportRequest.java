badec1339a013388229eabdad0f2a6e23d85ad9b
백재혁 <bjh010321@naver.com>
2025-01-18 06:47:46 +0900

    [hotfix] request dto 오류 수정

--- src/main/java/classfit/example/classfit/scoreReport/dto/request/CreateReportRequest.java
+++ src/main/java/classfit/example/classfit/scoreReport/dto/request/CreateReportRequest.java
@@ -6,26 +6,31 @@ import classfit.example.classfit.member.domain.Member;
 import classfit.example.classfit.scoreReport.domain.ScoreReport;
 import classfit.example.classfit.student.domain.Student;
 import jakarta.validation.constraints.NotNull;
-import java.time.LocalDate;
-import java.util.List;
 import org.springframework.format.annotation.DateTimeFormat;
 
-public record CreateReportRequest(Long mainClassId, Long subClassId, String reportName,
-                                  boolean includeAverage,
-                                  @DateTimeFormat(pattern = "yyyy-MM-dd") LocalDate startDate,
-                                  @DateTimeFormat(pattern = "yyyy-MM-dd") LocalDate endDate,
-                                  @NotNull(message = "시험 리스트를 선택해주세요.") List<Long> examIdList,
-                                  @NotNull(message = "종합 의견을 입력해주세요.")
-                                  String overallOpinion) {
+import java.time.LocalDate;
+import java.util.List;
 
-    public ScoreReport toEntity(SubClass subClass, MainClass mainClass,Student student, Member member) {
+public record CreateReportRequest
+    (
+        Long mainClassId, Long subClassId, String reportName,
+        Boolean includeAverage,
+        @DateTimeFormat(pattern = "yyyy-MM-dd") LocalDate startDate,
+        @DateTimeFormat(pattern = "yyyy-MM-dd") LocalDate endDate,
+        @NotNull(message = "시험 리스트를 선택해주세요.") List<Long> examIdList,
+        @NotNull(message = "종합 의견을 입력해주세요.")
+        String overallOpinion
+    ) {
+
+    public ScoreReport toEntity(SubClass subClass, MainClass mainClass, Student student, Member member) {
         return ScoreReport.builder()
-                .subClass(subClass)
-                .mainClass(mainClass)
-                .student(student)
-                .reportName(reportName)
-                .overallOpinion(overallOpinion)
-                .reportCreatedBy(member.getName())
-                .build();
+            .subClass(subClass)
+            .mainClass(mainClass)
+            .student(student)
+            .reportName(reportName)
+            .includeAverage(includeAverage)
+            .overallOpinion(overallOpinion)
+            .reportCreatedBy(member.getName())
+            .build();
     }
 }
